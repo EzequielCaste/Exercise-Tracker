19d29b5f9923b8925ba756dac25d51bd86dbd81f
Glitch (periodic-jonquil) <none>
2019-12-16 22:17:55 +0000

    ✂️🐁 Checkpoint ./server.js:8919222/1194

--- server.js
+++ server.js
@@ -14,18 +14,30 @@ mongoose.connect("mongodb+srv://eze:fcc456@cluster0-py5g6.mongodb.net/test?retry
   
 });
 
-
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+/*
+
+1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
+2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+3. I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will be the user object with also with the exercise fields added.
+4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
+5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
+
+*/
 
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get("/api/exercise/:new-user", function(req,res){
+  
+})
 
 // Not found middleware
 app.use((req, res, next) => {
